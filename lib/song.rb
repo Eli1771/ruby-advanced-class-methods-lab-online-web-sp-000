ebe8934cdb0a2e582d78d002f4942b51f846e9c4
elijahmm17 <elijahmm17@gmail.com>
2019-05-21 21:19:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,23 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create 
+    song = self.new 
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    named = self.new
+    named.name = name
+    named
+  end 
+  
+  def self.create_by_name(name)
+    named = self.new 
+    named.name = name 
+    named.save
+    named
+  end 
 end
